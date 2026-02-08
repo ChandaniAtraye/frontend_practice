f601c6af454f7510d2b594a208a7e2be1c97917c
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2026-02-08 22:40:55 +0530

    learned operators in js

---
+++ javascript/app.js
@@ -0,0 +1,4 @@
+let pencilPrice = 10;
+let penPrice = 20;
+// console.log("The total price is : ", pencilPrice+penPrice ,"rupees." );
+console.log(`The total price is : ${pencilPrice+penPrice} rupees.`) //  `this is a back ticks`
---
+++ javascript/operator.js
@@ -0,0 +1,28 @@
+// 1. arithmetic operators
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// 2.unary operators
+console.log(b++);//5
+console.log(++b);//7
+
+
+//3.assignment operators
+
+// 4. comparison operator
+// >,>=,<,<=,!=,==,===
+let x = 20;
+let y = "20"
+console.log(a>b);
+console.log(x==y); // comapres value not type
+console.log(x===y);// compares both value and type
+
+
+// 
+// 
