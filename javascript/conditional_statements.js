be64648c4a84c321bb3841fec470ebc3511e3485
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2026-02-08 22:41:24 +0530

    learned Conditional_statements

---
+++ javascript/conditional_statements.js
@@ -0,0 +1,35 @@
+// conditional statement
+//1.if_statement
+let age =18;
+if(age>=18){
+    console.log("you can vote");
+}
+  // practice question on if statement
+
+  //traffic light system
+
+  let color = "red"
+  if(color=="red"){
+    console.log("Stop light color is red");
+  }
+  if(color==="yellow light color is yellow"){
+    console.log("Slow Down");
+  }
+  if(color==="green light color is green"){
+    console.log("Go");
+  }
+
+
+  //2.else if -- 
+
+  let marks = 90;
+  if(marks>=80){
+    console.log("A+");
+  }else if(marks>=60){
+    console.log("A");
+  }else if(marks>=33){
+    console.log("B");
+  }else if(marks<33){
+    console.log("F");
+  }
+  
