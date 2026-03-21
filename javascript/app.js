190515e83d2d5a4ed50a6198ab78c4af3fbe0d35
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2026-03-21 23:27:36 +0530

    revised conditional statement

--- javascript/app.js
+++ javascript/app.js
@@ -2,3 +2,54 @@ let pencilPrice = 10;
 let penPrice = 20;
 // console.log("The total price is : ", pencilPrice+penPrice ,"rupees." );
 console.log(`The total price is : ${pencilPrice+penPrice} rupees.`) //  `this is a back ticks`
+console.log(`${pencilPrice}`)
+
+
+//grading statments
+
+let marks = 98;
+if(marks>=80){
+    console.log("A+");
+}
+else if(marks>=60){
+    console.log("A");
+}
+else if(marks>=33){
+    console.log("B");
+}
+else if(marks<33){
+    console.log("Appear for exam again");
+}
+
+let month="Apr";
+if(month==="Jan"){
+    console.log("Winter");
+}else if (month==="Apr"){
+    console.log("Summer");
+}
+
+
+//else statement
+let age = 19;
+if(age>=18){
+    console.log("You are eligible for the exam");
+}else{
+    console.log("You are not eligible for the exam");
+}
+
+// nested if statement
+
+let public_age = 15;
+
+if (public_age>=18){
+    console.log("You are eligible to vote");
+    if(public_age>=25){
+        console.log("you can drive, you can vote ,you can get driving license");
+    }else{
+        ("You cant get driving lices");
+    }
+}
+else{
+    console.log("You are not eligible");
+}
+   
