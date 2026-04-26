0a67f427f694b9ad4d3f3ff0e102047cf2d574a3
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2026-04-26 23:53:04 +0530

    done practice question and learned arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,29 @@
+
+// arrays : linear collection of things.
+// let student1 = " aman";
+// let student2 = " rajat";
+// let student3 = " raman";
+// console.log(student1,student2,student3);
+let students = ["aman","raman","rajat"];
+console.log(students);
+
+let nums = [2,4,6,8,];
+console.log(nums);
+
+let info = ["sharadha",23,89.9]; // mixed array
+console.log(info);
+console.log(info.length)
+console.log(info[0]);
+console.log(info[0][1]);
+console.log(info[0].length);
+
+//empty array
+let emArr = [];
+console.log(emArr);
+
+// arrays are mutable - values can change 
+let fruits =["mango","Apple","papaya"];
+fruits[0]="banana";
+console.log(fruits);
+fruits[10] ="lichi";
+console.log(fruits);
